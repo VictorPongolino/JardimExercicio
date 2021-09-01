9b652420fbdf166345ca1a072899f66e92241c8a
Victor Hugo Pongolino <victor.pongolino@hotmail.com>
2021-09-01 12:24:05 -0300

    adicionado documentação

--- src/Jardim.java
+++ src/Jardim.java
@@ -3,17 +3,35 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+/***
+ * Representa um Jardim que conterá todas as plantas.
+ */
 public class Jardim {
     private List<Planta> plantas = new ArrayList<>();
 
+    /***
+     * Adiciona uma nova planta no jardim.
+     * @param planta uma instância de Planta a ser adicionado.
+     */
     public void plantar(Planta planta) {
         this.plantas.add(planta);
     }
 
+    /***
+     * Remove uma planta
+     * @param planta a instância a ser removida.
+     * @return retorna verdadeiro se
+     */
     public boolean desplantar (Planta planta) {
         return this.plantas.remove(planta);
     }
 
+    /***
+     * Remove a primeira planta que encontrar com base em um nome.
+     * Não é Case sensitive.
+     * @param nome
+     * @return
+     */
     public boolean desplantar (String nome) {
         Optional<Planta> planta = this.plantas.stream()
                 .filter((Planta x) -> x.getNome().equalsIgnoreCase(nome))
@@ -26,6 +44,12 @@ public class Jardim {
         return false;
     }
 
+    /**
+     * Filtra e retorna todas as plantas com o nome informado.
+     * Ignora case sensitive.
+     * @param nome nome da planta
+     * @return retorna uma Lista de Plantas encontradas.
+     */
     public List<Planta> getPlantasByName (String nome) {
         List<Planta> plantas = this.plantas
                 .stream()
@@ -35,6 +59,10 @@ public class Jardim {
         return plantas;
     }
 
+    /***
+     * Retorna o numero total sem filtro se plantas plantadas.
+     * @return o número total de plantas plantadas.
+     */
     public int totalPlantas() {
         return this.plantas.size();
     }
--- src/MenuCommand.java
+++ src/MenuCommand.java
@@ -1,15 +1,29 @@
 import java.util.*;
 
+/***
+ * Uma classe abstrata que registra uma operação e sua breve descrição do que será realizada nesta operação.
+ * Não foi utilizado interface por conta de que em interfaces os campos são estáticos finais e o objetivo é poder
+ * cadastrar comandos de instâncias independentes.
+ */
 abstract class Command {
+    /***
+     * Uma descrição breve do que o comando irá realizar.
+     */
     public final String DESCRICAO;
     public Command (String descricao) {
         this.DESCRICAO = descricao;
     }
 
+    /***
+     * Cria um comando, uma vez buscado e encontrado a operação, será este método que será chamado.
+     */
     public abstract void create();
 }
 
-
+/***
+ * Um command de Jardim para registrar em forma de menus as operações a serem realizadas.
+ * O Objetivo é ENCAPSULAR as operações.
+ */
 public final class MenuCommand {
     private final Map<String, Command> SUB_MENUS;
     private final Jardim jardim;
@@ -19,7 +33,14 @@ public final class MenuCommand {
     }
 
     {
+        // Um Map temporário para registrar todos os conteúdos antes de depositar em um Map FINAL.
         final Map<String, Command> regCmd = new HashMap<>();
+        /*
+         * Operações Encapsuladas em um HashMap;
+         * Lembre-se de que não é aceito valores duplicados.
+         * */
+        
+        // Comando
         regCmd.put("1", new Command("Para adicionar uma planta nova") {
             @Override
             public void create() {
@@ -32,6 +53,7 @@ public final class MenuCommand {
             }
         });
 
+        // Comando
         regCmd.put("2", new Command("Para remover uma planta pelo nome") {
             @Override
             public void create() {
@@ -46,6 +68,7 @@ public final class MenuCommand {
             }
         });
 
+        // Comando
         regCmd.put("3", new Command("Para pegar o número total de plantas pelo nome") {
             @Override
             public void create() {
@@ -57,6 +80,7 @@ public final class MenuCommand {
             }
         });
 
+        // Comando
         regCmd.put("4", new Command("Para pegar o número total de plantas") {
             @Override
             public void create() {
@@ -64,16 +88,25 @@ public final class MenuCommand {
             }
         });
 
+        // Registra todos os comandos em uma lista final não modificável.
         SUB_MENUS = Collections.unmodifiableMap(regCmd);
     }
 
+
+    /***
+     * Imprime o catalogo do menu.
+     */
     public void printMenu() {
         for (Map.Entry<String, Command> it : this.SUB_MENUS.entrySet()) {
             System.out.println(it.getKey() + " - " + it.getValue().DESCRICAO);
         }
     }
 
-
+    /***
+     * Chama um comando encapsulado.
+     * @param sub_menu uma operação a ser buscada
+     * @return verdadeiro se a operação foi encontrada, false do contrário.
+     */
     public boolean callSubmenu (String sub_menu) {
         Command command = this.SUB_MENUS.get(sub_menu);
         if (command == null) {
