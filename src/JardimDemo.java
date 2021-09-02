cc7655762d2d6ebfe1b95c3038fe95307a68add5
Victor Hugo Pongolino <victor.pongolino@hotmail.com>
2021-09-02 10:04:00 -0300

    removido variável não utilizada

--- src/JardimDemo.java
+++ src/JardimDemo.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class JardimDemo {
     public static void main(String[] args) {
-        Jardim jardim = new Jardim();
         System.out.println("Bem-Vindo(A) ao Colheita Feliz!");
 
         Scanner sc = new Scanner(System.in);
